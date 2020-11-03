09c358e66f7f960eddb39bea443b74ee640664fb
Tushar Nikam <champ96k@gmail.com>
2020-11-03 14:47:49 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:coronavirus_tracking_app/UI/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Coronavirus Outbreak",
+      home: HomePage(),
+    );
+  }
+}
